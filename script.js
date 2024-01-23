c6ff8c7975089addca150571e12f9738950169bc
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-23 23:34:25 +0000

    Add .vscode settings, index.html, script.js, styles.css, KOF.png, and kof2.png files

---
+++ script.js
@@ -0,0 +1,20 @@
+const menuBtn = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".bottom-nav");
+// const navOverlay = document.querySelector(".overlay");
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  navLinks.classList.toggle("active");
+//   navOverlay.classList.toggle("active");
+  menuBtn.classList.toggle("cross");
+}
+
+const navbarLinks = document.querySelectorAll("[data-navbar-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+    menuBtn.classList.toggle("cross");
+  });
+}
