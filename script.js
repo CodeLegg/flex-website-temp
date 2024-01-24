126df090d281c2748f51af8c64104008cc77542b
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-24 05:40:32 +0000

    Refactor mobile menu functionality

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const menuBtn = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".bottom-mobile-menu");
+
+
+
+const mobileMenuBtn = document.querySelector(".hamburger");
+const mobileMenuDropdown = document.querySelector(".bottom-mobile-menu");
 // const navOverlay = document.querySelector(".overlay");
 
-menuBtn.addEventListener("click", toggleMenu);
+mobileMenuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  navLinks.classList.toggle("active");
+  mobileMenuDropdown.classList.toggle("active");
 //   navOverlay.classList.toggle("active");
-  menuBtn.classList.toggle("cross");
+  mobileMenuBtn.classList.toggle("cross");
 }
+
+
+
 
 const navbarLinks = document.querySelectorAll("[data-navbar-link]");
 
 for (let i = 0; i < navbarLinks.length; i++) {
   navbarLinks[i].addEventListener("click", function () {
-    navLinks.classList.toggle("active");
-    menuBtn.classList.toggle("cross");
+    mobileMenuDropdown.classList.toggle("active");
+    mobileMenuBtn.classList.toggle("cross");
   });
 }
