7b1dbdcc0eab0f21babc7da2c89fe8b160040388
CodeLegg <michaelraymondlegg@gmail.com>
2024-01-29 03:33:58 +0000

    Toggle mobile menu and handle body overflow

--- script.js
+++ script.js
@@ -3,15 +3,19 @@
 
 const mobileMenuBtn = document.querySelector(".hamburger");
 const mobileMenuDropdown = document.querySelector(".bottom-mobile-menu");
-// const navOverlay = document.querySelector(".overlay");
+const navOverlay = document.querySelector(".overlay");
 
 mobileMenuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   mobileMenuDropdown.classList.toggle("active");
-//   navOverlay.classList.toggle("active");
+  navOverlay.classList.toggle("active");
   mobileMenuBtn.classList.toggle("cross");
+
+  // Toggle a class on the body to handle overflow
+  document.body.classList.toggle("no-scroll", mobileMenuDropdown.classList.contains("active"));
 }
+
 
 
 
@@ -23,9 +27,8 @@ let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
-  
   if (currentScroll <= 0) {
-    body.classList.remove("scroll-up");
+    body.classList.add("scroll-up");
   }
   if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
     body.classList.remove("scroll-up");
